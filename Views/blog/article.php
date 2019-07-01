c827c14e893b219976048610097a42657c49ff87
P@piH@ck3R <mssmbaye@gmail.com>
2019-07-01 08:28:47 +0000

    refacto article.php file :)

--- Views/blog/article.php
+++ Views/blog/article.php
@@ -5,14 +5,14 @@ ob_start();
 ?>
 
 <div id="contenu">
-<?php if($article != null) { ?>
-    <h1><?= $article->getTitre() ?></h1>
-    <span>Publié le <?= $article->getDateCreation() ?></span>
-    <p><?= $article->getContenu() ?></p>
-<?php } else { ?>
-	<meta http-equiv="refresh" content="3; url=index.php">
-	<h1>Aucun article trouvé !</h1>
-<?php } ?>
+    <?php if($article != null) { ?>
+        <h1><?= $article->getTitre() ?></h1>
+        <span>Publié le <?= $article->getDateCreation() ?></span>
+        <p><?= $article->getContenu() ?></p>
+    <?php } else { ?>
+        <meta http-equiv="refresh" content="3; url=index.php">
+        <h1>Aucun article trouvé !</h1>
+    <?php } ?>
 </div>
 <?php
 $content = ob_get_clean(); 
